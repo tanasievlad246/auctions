e2bc4e999156dd3d811d599e5995e321d4a51305
tanasievlad246 <tanasievladcristian@gmail.com>
2024-10-24 23:09:53 +0300

    Fixed docker compose to let nestjs connect to the database and check for code changes

--- auctions-service/src/app.module.ts
+++ auctions-service/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuctionsController } from './auctions/auctions.controller';
+import { AuctionsService } from './auctions/auctions.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      schema: 'auctions',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    })
+  ],
+  controllers: [AuctionsController],
+  providers: [AuctionsService],
 })
 export class AppModule {}
---
+++ auctions-service/src/auctions/entities/auction.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Auction {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    @Column()
+    title: string;
+    @Column()
+    description: string;
+    @Column()
+    startDate: Date;
+    @Column()
+    endDate: Date;
+    @Column()
+    startingTransportDate: Date;
+    @Column()
+    endingTransportDate: Date;
+    @Column()
+    startingPrice: number;
+}
