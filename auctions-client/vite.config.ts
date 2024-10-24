ad7c80a993bc2198becec050a9e14c47732a0937
tanasievlad246 <tanasievladcristian@gmail.com>
2024-10-24 15:22:10 +0300

    Updated sveltekit to expose svelte app

--- auctions-client/vite.config.ts
+++ auctions-client/vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0',
+		port: 5173,
+		strictPort: true,
+		watch: {
+		  usePolling: true
+		}
+	}
 });
