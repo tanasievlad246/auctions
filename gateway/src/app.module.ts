3ae25c7bcf41b9abf7c6b6a8668fbbb988c74806
tanasievlad246 <tanasievladcristian@gmail.com>
2025-01-04 01:07:52 +0200

    Added gateway config in gateway/app.module.ts

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        playground: process.env.NODE_ENV !== 'production',
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'auctions', url: 'http://auctions-service:3001/graphql' },
+          ],
+        }),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
