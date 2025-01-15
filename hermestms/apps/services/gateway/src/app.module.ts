c4996241f28789120f89e6a8e2d779da778bf6a6
vladtanasiedns <tanasie.vlad@dandsltd.com>
2025-01-15 23:02:16 +0200

    Adding graphql to the new turborepo setup

--- hermestms/apps/services/gateway/src/app.module.ts
+++ hermestms/apps/services/gateway/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        playground: process.env.NODE_ENV !== 'production',
+        debug: process.env.NODE_ENV !== 'production',
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            // { name: 'auctions', url: 'http://auctions-service:3001/graphql' },
+            // { name: 'users', url: 'http://users-service:3002/graphql' },
+          ],
+          pollIntervalInMs: 1000,
+          subgraphHealthCheck: true,
+          logger: console,
+        }),
+        debug: process.env.NODE_ENV !== 'production',
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
